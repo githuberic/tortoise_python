9e151f7f05c136139e957c9ee3656181e1a0f24d
lgq <lgq_1314@126.com>
2024-04-04 01:31:17 +0800

    add processing_pool async

---
+++ concurrent/multiple_process/processing_pool_queue.py
@@ -0,0 +1,30 @@
+import os
+import random
+import time
+from multiprocessing import Manager, Process, Queue, Pool
+
+
+def write(q):
+    print(f"Write进程{os.getpid()}开始执行,父进程={os.getppid()}")
+    for val in ['A', 'B', 'C', 'D', 'E', 'F']:
+        q.put(val)
+
+
+def read(q):
+    print(f"Read进程{os.getpid()}开始执行,父进程={os.getppid()}")
+    while True:
+        if not q.empty():
+            val = q.get(True)
+            print(f"Get {val} from queue")
+        else:
+            break
+
+
+if __name__ == '__main__':
+    q = Manager().Queue()
+    po = Pool()
+    po.apply_async(write, (q,))
+    po.apply_async(read, (q,))
+    po.close()
+    po.join()
+    print(f">>> {os.getpid()}Done")
