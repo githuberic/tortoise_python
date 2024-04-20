951cc0ece0b012d706a630fd8ec197acbaa95aa8
lgq <lgq_1314@126.com>
2024-04-21 00:21:59 +0800

    add multiple inhert exec

---
+++ oo/multiple_inhert_exec.py
@@ -0,0 +1,57 @@
+# 类定义
+class People:
+    # 定义基本属性
+    name = ''
+    age = 0
+    # 定义私有属性,私有属性在类外部无法直接进行访问
+    _weight = 0
+
+    # 定义构造方法
+    def __init__(self, n, a, w):
+        self.name = n
+        self.age = a
+        self._weight = w
+
+    def speak(self):
+        print("%s 说: 我 %d 岁。" % (self.name, self.age))
+
+
+# 单继承示例
+class Student(People):
+    grade = ''
+
+    def __init__(self, n, a, w, g):
+        # 调用父类的构函
+        People.__init__(self, n, a, w)
+        self.grade = g
+
+    # 覆写父类的方法
+    def speak(self):
+        print("%s 说: 我 %d 岁了，我在读 %d 年级" % (self.name, self.age, self.grade))
+
+
+# 另一个类，多继承之前的准备
+class Speaker:
+    topic = ''
+    name = ''
+
+    def __init__(self, n, t):
+        self.name = n
+        self.topic = t
+
+    def speak(self):
+        print("我叫 %s，我是一个演说家，我演讲的主题是 %s" % (self.name, self.topic))
+
+
+# 多继承
+class Sampler(Speaker, Student):
+    a = ''
+
+    def __init__(self, n, a, w, g, t):
+        Student.__init__(self, n, a, w, g)
+        Speaker.__init__(self, n, t)
+
+
+if __name__ == "__main__":
+    sampler = Sampler("Tim", 25, 80, 4, "Python 从入门到放弃")
+    sampler.speak()
--- oo/oo_example.py
+++ oo/oo_example.py
@@ -1,21 +1,26 @@
 class Person:
     def __init__(self, name, age):
-        self.__name = name
-        self.__age = age
+        self._name = name
+        self._age = age
+
+    def prt_self(this):
+        print(this)
+        print(this.__class__)
 
     def GetUserInfo(self):
-        return "{}的年龄为{}".format(self.__name, self.__age)
+        return "{}的年龄为{}".format(self._name, self._age)
 
     def __str__(self):
-        return "{}的年龄为{}".format(self.__name, self.__age)
+        return "{}的年龄为{}".format(self._name, self._age)
 
     def setAge(self, age):
         if 0 < age < 120:
-            self.__age = age
+            self._age = age
 
 
 def start():
     person = Person('lgq', 18)
+    person.prt_self()
     print(person.GetUserInfo())
 
     print(person.setAge(19))
